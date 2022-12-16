55ef025f3cc595a894f46bf517bfee9da066c16d
Villads Frederiksen <villadsfrederiksen1@gmail.com>
2022-12-16 09:17:11 +0100

    Created movement

--- main.js
+++ main.js
@@ -1,26 +1,55 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
-var x = 21;
-var y = 21;
-var gS = 20;
+var x = 11;
+var y = 11;
+var gS = 50;
 
 window.addEventListener("load", () => {
     canvas.width = x*gS;
     canvas.height = y*gS;
     Snake = new Snake(5)
-    Snake.draw()
+    draw()
 })
+
+function draw(){
+    clear()
+    Snake.draw()
+    Snake.move()
+
+    setTimeout(draw, 100)
+}
+
+
 
 class Snake{
     constructor(length){
         this.length = length;
+        this.snake = []
+        this.vx = 1;
+        this.vy = 0;
+        for (let i = 0; i < this.length; i++) {
+            this.snake[i] = [i, Math.floor(y-y/2)]
+        }
+        this.snake.reverse()
     }
     draw(){
-        for (let i = 0; i < this.length; i++) {
-            block(i,Math.floor(y-y/2), "#00acff")  
+        for(let i = 0; i < this.length; i++) {
+            block(this.snake[i][0], this.snake[i][1], "#00acff")
         }
     }
+    move(){
+        this.snake.unshift([this.snake[0][0]+this.vx, this.snake[0][1]+this.vy])
+        this.snake.pop()
+    }
+}
+
+function clear(){
+    ctx.beginPath()
+    ctx.rect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = "white";
+    ctx.fill()
+    ctx.closePath();
 }
 
 function block(x, y, color){
@@ -33,9 +62,19 @@ function block(x, y, color){
 
 document.addEventListener("keydown", (Event) => {
     if (Event.key == "ArrowLeft") {
-        console.log("left")
+        Snake.vx = -1;
+        Snake.vy = 0;
     }
     if (Event.key == "ArrowRight") {
-        console.log("right")
+        Snake.vx = 1;
+        Snake.vy = 0;
+    }
+    if (Event.key == "ArrowUp") {
+        Snake.vx = 0;
+        Snake.vy = -1;
+    }
+    if (Event.key == "ArrowDown") {
+        Snake.vx = 0;
+        Snake.vy = 1;
     }
 })
